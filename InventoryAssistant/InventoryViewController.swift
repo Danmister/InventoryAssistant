06846ff3d0db4bca331788e4bd49f6f39e485739
Danmister <dcorrea012@gmail.com>
2017-01-04 20:40:53 -0500

    made some changes to add page

---
+++ InventoryAssistant/InventoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InventoryViewController.swift
+//  InventoryAssistant
+//
+//  Created by Daniel Correa on 1/4/17.
+//  Copyright © 2017 Repel. All rights reserved.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
