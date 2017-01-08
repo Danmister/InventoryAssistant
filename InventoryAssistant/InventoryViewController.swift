a66dc0b400bd3498cd8adbef763357507b1afc03
Danmister <dcorrea012@gmail.com>
2017-01-07 21:14:30 -0500

    Finished the app

--- InventoryAssistant/InventoryViewController.swift
+++ InventoryAssistant/InventoryViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class InventoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var addUpDateButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var itemImageView: UIImageView!
     
     var imagePicker = UIImagePickerController()
+    var item : Item? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        
+        if item != nil {
+            itemImageView.image = UIImage(data: item!.image as! Data)
+            titleTextField.text = item!.title
+            
+            addUpDateButton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
     }
     
     @IBAction func photosTapped(_ sender: Any) {
@@ -39,22 +52,44 @@ class InventoryViewController: UIViewController, UIImagePickerControllerDelegate
     
     @IBAction func cameraTapped(_ sender: Any) {
         
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
     @IBAction func addTapped(_ sender: Any) {
         
+        if item != nil {
+            
+            item!.title = titleTextField.text
+            item!.image = UIImagePNGRepresentation(itemImageView.image!) as NSData?
+            
+        } else {
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let item = Item(context: context)
+            item.title = titleTextField.text
+            item.image = UIImagePNGRepresentation(itemImageView.image!) as NSData?
+            
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+
+        navigationController!.popViewController(animated: true)
+
+    }
+    
+    @IBAction func deleteTapped(_ sender: Any) {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        let item = Item(context: context)
-        item.title = titleTextField.text
-        item.image = UIImagePNGRepresentation(itemImageView.image!) as NSData?
+        context.delete(item!)
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         navigationController!.popViewController(animated: true)
 
     }
-    
     
 }
