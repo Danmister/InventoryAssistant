dd30dfe10a9d196158f96a0679c65b9143d70907
Danmister <dcorrea012@gmail.com>
2017-01-07 19:08:23 -0500

    We can create item objects and pull them out from coredata

--- InventoryAssistant/InventoryViewController.swift
+++ InventoryAssistant/InventoryViewController.swift
@@ -8,21 +8,53 @@
 
 import UIKit
 
-class InventoryViewController: UIViewController {
+class InventoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var itemImageView: UIImageView!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
+    
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        itemImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cameraTapped(_ sender: Any) {
+        
+        
     }
+    
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let item = Item(context: context)
+        item.title = titleTextField.text
+        item.image = UIImagePNGRepresentation(itemImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+
     }
+    
     
 }
